4edb153e205cbe8c34da92eb729c9f3d5fa90be7
Keun-Bae Lee <keunbai@yahoo.com>
2020-12-05 09:52:53 +0900

    #6 modified in branch_3rd/remote repo

--- first.py
+++ first.py
@@ -1,3 +1,4 @@
 print ("1st sentence at 1st file in main branch")
-print ("2nd sentence at 1st file in branch_1st")
-print ("3rd sentence at 1st file in branch_3rd")
+print ("1st sentence at 1st file in branch_1st")
+print ("1st sentence at 1st file in branch_3rd")
+print ("1st sentence at 1st file in branch_3rd/remote repo")
